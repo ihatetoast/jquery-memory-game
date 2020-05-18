f22edbc51b2a5cca02283e201fe1ca10118f9992
Katy <ihatetoast@gmail.com>
2020-05-18 16:14:06 -0500

    card deck styled to 3 break points

--- app/js/main.js
+++ app/js/main.js
@@ -4,27 +4,26 @@
 // });
 
 const imageArray = [
-  "images/circle1.jpg",
-  "images/circle2.jpg",
-  "images/circle3.jpg",
-  "images/circle4.jpg",
-  "images/cube1.jpg",
-  "images/cube2.jpg",
-  "images/octogon1.jpg",
-  "images/octogon2.jpg",
-  "images/rhombi1.jpg",
-  "images/rhombi2.jpg",
-  "images/square1.jpg",
-  "images/square2.jpg",
-  "images/star1.jpg",
-  "images/star2.jpg",
-  "images/star3.jpg",
-  "images/star4.jpg",
-  "images/triangle1.jpg",
-  "images/triangle2.jpg",
-  "images/triangle3.jpg",
-  "images/triangle4.jpg",
-
+  "circle1",
+  "circle2",
+  "circle3",
+  "circle4",
+  "cube1",
+  "cube2",
+  "octogon1",
+  "octogon2",
+  "rhombi1",
+  "rhombi2",
+  "square1",
+  "square2",
+  "star1",
+  "star2",
+  "star3",
+  "star4",
+  "triangle1",
+  "triangle2",
+  "triangle3",
+  "triangle4",
       ];
 
 
@@ -33,17 +32,27 @@ const imageArray = [
 
 
 $(document).ready(function(){
-  function ResetGame(){
-    console.log("reset game fired");
-  }
 
-  // shuffle array. get 8 once shuffled.
-  const subArr = imageArray.sort(()=>0.5 - Math.random()).slice(0,18);
+  // duplicate imageArray
+  const workingImgArr = [...imageArray]
+  // shuffle and then take 12 (or whatever)
+  const subArr = workingImgArr.sort(()=>0.5 - Math.random()).slice(0,12);
   // double up the array so there are two of each card. shuffle again.
   const gameArr = [...subArr, ...subArr].sort(()=>0.5 - Math.random());
+ 
+
   $(gameArr).each(function(i, e){
-    $("#images").append(`<div class="img-card col-3  col-md-2"><img src='./${e}' alt='shape' /></div>`)
+    $("#images").append(`<div id=${e} class="img-card" ><img src='./images/${e}.jpg' alt='shape' class="card-img"/></div>`)
   })
+
+  // RESET GAME: RESHUFFLE AND HIDE CARDS
+  function ResetGame(){
+    console.log("reset game fired");
+    $(".card-img").css("display", "none")
+  }
+
+  
+  // $(".card-img").css("visibility", "hidden")
   $("#reset").on('click', function(){
     ResetGame();
   })
