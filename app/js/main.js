a8f11d369c32d8a484cd3d7f1d4631ca561fb02d
Katy <ihatetoast@gmail.com>
2020-05-18 18:14:01 -0500

    card checking logic started. check id and image source. no comparisons yet

--- app/js/main.js
+++ app/js/main.js
@@ -4,57 +4,89 @@
 // });
 
 const imageArray = [
-  "circle1",
-  "circle2",
-  "circle3",
-  "circle4",
-  "cube1",
-  "cube2",
-  "octogon1",
-  "octogon2",
-  "rhombi1",
-  "rhombi2",
-  "square1",
-  "square2",
-  "star1",
-  "star2",
-  "star3",
-  "star4",
-  "triangle1",
-  "triangle2",
-  "triangle3",
-  "triangle4",
+  "shape1",
+  "shape2",
+  "shape3",
+  "shape4",
+  "shape5",
+  "shape6",
+  "shape7",
+  "shape8",
+  "shape9",
+  "shape10",
+  "shape11",
+  "shape12",
+  "shape13",
+  "shape14",
+  "shape15",
+  "shape16",
+  "shape17",
+  "shape18",
+  "shape19",
+  "shape20",
       ];
 
 
 
 // jquery code
-
+let imgName1 = "";
+let divId = "";
+let currentImg="";
 
 $(document).ready(function(){
 
-  // duplicate imageArray
+ 
+ function shuffleImages(){
+  console.log("shuffleImages is fired");
+ // duplicate imageArray
   const workingImgArr = [...imageArray]
   // shuffle and then take 12 (or whatever)
   const subArr = workingImgArr.sort(()=>0.5 - Math.random()).slice(0,12);
   // double up the array so there are two of each card. shuffle again.
   const gameArr = [...subArr, ...subArr].sort(()=>0.5 - Math.random());
- 
-
   $(gameArr).each(function(i, e){
-    $("#images").append(`<div id=${e} class="img-card" ><img src='./images/${e}.jpg' alt='shape' class="card-img"/></div>`)
-  })
+      $("#images").append(`<div id=${i}-${e} class="img-card" ><img src='./images/${e}.jpg' alt='shape' class="card-img"/></div>`)
+      
+    })
+ }
+ 
+// CHECK IF THE CARDS MATCH. 
+// IF THEY DO, KEEP THEM SHOWING. IF THEY DO NOT, HIDE
+
+function showCard(){
+ 
+  const id=$(this).attr("id");
+  if($(`#${id} img`).is(":hidden")){
+    $(`#${id} img`).css("display", "block");
+
+    if(!imgName1){
+      divId = id;
+      imgName1 = $(`#${id} img`).attr('src');
+    } else{
+      currentImg = $(`#${id} img`).attr('src');
+    }
+    console.log(`divId is ${divId}`);
+    console.log(`imgName1 is ${imgName1}`);
+    console.log(`currentImg is ${currentImg}`);
+    
+  }
+}
 
   // RESET GAME: RESHUFFLE AND HIDE CARDS
   function ResetGame(){
     console.log("reset game fired");
-    $(".card-img").css("display", "none")
+    // $(".card-img").css("display", "none");
+    $("#images").empty();
+    shuffleImages();
   }
 
   
-  // $(".card-img").css("visibility", "hidden")
+  shuffleImages();
+
   $("#reset").on('click', function(){
     ResetGame();
-  })
+  });
+  $("#images div").click(showCard)
+
   
 });
